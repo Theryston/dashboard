92415a36b185fc9f96f90605407474c0437cd6bb
= <=>
2023-10-21 14:26:28 -0300

    feat: header

--- app/profile/Header/index.tsx
+++ app/profile/Header/index.tsx
@@ -1,8 +1,23 @@
 "use client";
 
 import * as S from "./styles";
-// import SimpleArrowLeftSvg from '../../../'
+import SimpleArrowLeftSvg from "../../../assets/icons/simple-arrow-left.svg";
+import LogoFSvg from "../../../assets/icons/logo-f.svg";
+import BoySvg from "../../../assets/icons/boy.svg";
+import ArrowDownCultureSvg from "../../../assets/icons/arrow-down-culture.svg";
 
 export default function Header() {
-  return <S.Container>{/* <S.BackIcon alt="Go Back" src={} /> */}</S.Container>;
+  return (
+    <S.Container>
+      <S.WrapperNavigation>
+        <S.BackIcon alt="Go Back" src={SimpleArrowLeftSvg} />
+        <S.Logo alt="logo" src={LogoFSvg} />
+      </S.WrapperNavigation>
+      <S.UserInfoWrapper>
+        <S.ManIcon src={BoySvg} alt="Icon Man" />
+        <p>Hello Adobe</p>
+        <S.ArrowDownIcon src={ArrowDownCultureSvg} alt="Down" />
+      </S.UserInfoWrapper>
+    </S.Container>
+  );
 }
--- app/profile/Header/styles.ts
+++ app/profile/Header/styles.ts
@@ -7,10 +7,82 @@ export const Container = styled.div`
   width: 100%;
   background: #ffffff;
   border-bottom: 1px #dddfe4 solid;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 36px;
+
+  @media (max-width: 1440px) {
+    height: 67px;
+    padding-left: 35px;
+  }
 `;
 
-export const BackIcon = styled(Image)``;
+export const WrapperNavigation = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 50%;
+`;
 
-export const Logo = styled(Image)``;
+export const BackIcon = styled(Image)`
+  width: 25px;
+  height: auto;
 
-export const UserInfoWrapper = styled.div``;
+  @media (max-width: 1440px) {
+    width: 19px;
+  }
+`;
+
+export const Logo = styled(Image)`
+  width: 53px;
+  height: auto;
+
+  @media (max-width: 1440px) {
+    width: 40px;
+  }
+`;
+
+export const UserInfoWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 314px;
+  height: 100%;
+  border-left: 1px #dddfe4 solid;
+
+  @media (max-width: 1440px) {
+    width: 231px;
+  }
+
+  p {
+    font-size: 18px;
+    color: #1a1a1a;
+
+    @media (max-width: 1440px) {
+      font-size: 13px;
+    }
+  }
+`;
+
+export const ManIcon = styled(Image)`
+  height: 20px;
+  width: auto;
+  margin-right: 15px;
+
+  @media (max-width: 1440px) {
+    height: 15px;
+    margin-right: 11px;
+  }
+`;
+
+export const ArrowDownIcon = styled(Image)`
+  margin-left: 22px;
+  height: auto;
+  width: 14px;
+
+  @media (max-width: 1440px) {
+    margin-left: 14px;
+    width: 11px;
+  }
+`;
